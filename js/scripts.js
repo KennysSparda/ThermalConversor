f2a05dcc31951970675ae4b582c05725696b0fd5
swygax <kennykysv@gmail.com>
2022-01-13 12:30:03 -0300

    modified index.html for calcule on change in Select and added in js/script.js functionality to display the thermal scale (ex: ºC, ºF, K)

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function convert(event) {
     // Variables input and output
     let input = parseFloat(document.getElementById("thermInput").value);
     let output;
+    let thermalScale = "";
     let selectInput = document.getElementById("selectInput").value;
     let selectOutput = document.getElementById("selectOutput").value;
     
@@ -14,11 +15,13 @@ function convert(event) {
                 // C >> F
                 case "fahrenheit":
                     output = ( input * 9 / 5 ) + 32;
+                    thermalScale = "ºF";
                 break;
 
                 // C >> K
                 case "kelvin":
                     output = input + 273.15
+                    thermalScale = "K";
                 break;
 
                 // C >> C
@@ -33,11 +36,13 @@ function convert(event) {
                 // F >> C
                 case "celsius":
                     output = ( input - 32 ) * 5 / 9;
+                    thermalScale = "ºC";
                 break;
 
                 // F >> K
                 case "kelvin":
                     output = ( input - 32 ) * 5 / 9 + 273.15;
+                    thermalScale = "K";
                 break;
 
                 // F >> F
@@ -52,21 +57,24 @@ function convert(event) {
                 // K >> C
                 case "celsius":
                     output = input - 273.15;
+                    thermalScale = "ºC";
                 break;
 
                 // K >> F
                 case "fahrenheit":
                     output = ( input - 273.15 ) * 9 / 5 + 32;
+                    thermalScale = "ºF";
                 break;
 
                 // K >> K
                 default:
                     output = 0;
                     alert("looks like you're trying to troll me");
+                    thermalScale = "K";
             }
             break;
     }
-    output = output.toFixed(2);
+    output = output.toFixed(2) + thermalScale;
     document.getElementById("thermOutput").value = output;
 }
 
